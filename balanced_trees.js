4f13f7174a84ea67d41d58939d5b70ca2b2c35c6
plzusername <MohamedFaris2210@gmail.com>
2024-04-28 16:15:02 +0300

    Add function to create and set binary tree

--- balanced_trees.js
+++ balanced_trees.js
@@ -6,8 +6,30 @@ const Node = (data, left, right) => {
     }
 }
 
-const Tree = (treeTemplate) =>{
+const Tree = () =>{
     return {
-        root: null
+        root: null,
+        makeTree(treeTemplate){
+            if(treeTemplate.length == 0) return null
+
+            if(treeTemplate.length == 1) return Node(treeTemplate[0], null, null)
+
+            const arrayMidPoint = treeTemplate[Math.floor(treeTemplate.length / 2)]
+            const leftArrayHalf = treeTemplate.slice(0, Math.floor(treeTemplate.length / 2))
+            const rightArrayHalf = treeTemplate.slice(Math.floor(treeTemplate.length / 2) + 1, treeTemplate.length)
+
+            const tree = Node(arrayMidPoint, this.makeTree(leftArrayHalf), this.makeTree(rightArrayHalf))
+
+            return tree
+        },
+        implementTree(treeTemplate){
+            this.root = this.makeTree(treeTemplate)
+        }
     }
 }
+
+const myTree = Tree()
+
+myTree.implementTree([1,2,3,4,5,6,7])
+
+console.log(myTree.root)
